1063d9943087aaba746e3cfd03eb44c11d75ea77
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2025-04-03 21:10:21 +0200

    First commit

---
+++ public/register.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require "../database/connection.php";
+
+$email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+$password = $_POST["password"];
+$confirm_password = $_POST["confirm-password"];
+
+if ($password === $confirm_password) {
+    $check_account = $conn->prepare("SELECT * FROM account WHERE email = :email");
+    $check_account->bindParam(":email", $email);
+    $check_account->execute();
+
+    if ($check_account->rowCount() === 0) {
+        $encrypted_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $create_account = $conn->prepare("INSERT INTO account (email, password) VALUES (:email, :password)");
+        $create_account->bindParam(":email", $email);
+        $create_account->bindParam(":password", $encrypted_password);
+        $create_account->execute();
+
+        $_SESSION["success"] = "Registratie is gelukt, log nu in:";
+        header("Location: login-form.php");
+        exit();
+    } else {
+        $_SESSION["message"] = "Dit e-mailadres is al in gebruik.";
+        $_SESSION["email"] = htmlspecialchars($email);
+        header("Location: register-form.php");
+        exit();
+    }
+} else {
+    $_SESSION["message"] = "Wachtwoorden komen niet overeen.";
+    $_SESSION["email"] = htmlspecialchars($email);
+    header("Location: register-form.php");
+    exit();
+}

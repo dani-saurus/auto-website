84e4d19b82e0b24bb420d2f423268048777dd7a5
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2025-05-12 14:45:27 +0200

    Url links fixed so they are all relative
    Relative to current url rather than from domain

--- pages/home.php
+++ pages/home.php
@@ -5,15 +5,15 @@
                 <h2>Hét platform om een auto te huren</h2>
                 <p>Snel en eenvoudig een auto huren. Natuurlijk voor een lage prijs.</p>
                 <a href="#" class="button-primary">Huur nu een auto</a>
-                <img src="../assets/images/car-rent-header-image-1.png" alt="">
-                <img src="../assets/images/header-circle-background.svg" alt="" class="background-header-element">
+                <img src="./assets/images/car-rent-header-image-1.png" alt="">
+                <img src="./assets/images/header-circle-background.svg" alt="" class="background-header-element">
             </div>
             <div class="advertorial">
                 <h2>Wij verhuren ook bedrijfswagens</h2>
                 <p>Voor een vaste lage prijs met prettig voordelen.</p>
                 <a href="#" class="button-primary">Huur een bedrijfswagen</a>
-                <img src="../assets/images/car-rent-header-image-2.png" alt="">
-                <img src="../assets/images/header-block-background.svg" alt="" class="background-header-element">
+                <img src="./assets/images/car-rent-header-image-2.png" alt="">
+                <img src="./assets/images/header-block-background.svg" alt="" class="background-header-element">
 
             </div>
         </div>
@@ -32,13 +32,13 @@
                 </div>
                 <img src="assets/images/products/car%20(<?= $i ?>).svg" alt="">
                 <div class="car-specification">
-                    <span><img src="../assets/images/icons/gas-station.svg" alt="">90l</span>
-                    <span><img src="../assets/images/icons/car.svg" alt="">Schakel</span>
-                    <span><img src="../assets/images/icons/profile-2user.svg" alt="">2 Personen</span>
+                    <span><img src="./assets/images/icons/gas-station.svg" alt="">90l</span>
+                    <span><img src="./assets/images/icons/car.svg" alt="">Schakel</span>
+                    <span><img src="./assets/images/icons/profile-2user.svg" alt="">2 Personen</span>
                 </div>
                 <div class="rent-details">
                     <span><span class="font-weight-bold">€249,00</span> / dag</span>
-                    <a href="/car-detail" class="button-primary">Bekijk nu</a>
+                    <a href="./car-detail" class="button-primary">Bekijk nu</a>
                 </div>
             </div>
         <?php endfor; ?>
@@ -55,13 +55,13 @@
                 </div>
                 <img src="assets/images/products/car%20(<?= $i ?>).svg" alt="">
                 <div class="car-specification">
-                    <span><img src="../assets/images/icons/gas-station.svg" alt="">90l</span>
-                    <span><img src="../assets/images/icons/car.svg" alt="">Schakel</span>
-                    <span><img src="../assets/images/icons/profile-2user.svg" alt="">2 People</span>
+                    <span><img src="./assets/images/icons/gas-station.svg" alt="">90l</span>
+                    <span><img src="./assets/images/icons/car.svg" alt="">Schakel</span>
+                    <span><img src="./assets/images/icons/profile-2user.svg" alt="">2 People</span>
                 </div>
                 <div class="rent-details">
                     <span><span class="font-weight-bold">€249,00</span> / dag</span>
-                    <a href="/car-detail" class="button-primary">Bekijk nu</a>
+                    <a href="./car-detail" class="button-primary">Bekijk nu</a>
                 </div>
             </div>
         <?php endfor; ?>
